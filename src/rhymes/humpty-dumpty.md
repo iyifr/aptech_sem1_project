222287e37751cc78e3ce2afb0dceed4ad5feb4fc
Iyimide Adekile <adekileiyimide@gmail.com>
2024-02-21 17:49:46 +0100

    Rhymes

--- src/rhymes/humpty-dumpty.md
+++ src/rhymes/humpty-dumpty.md
@@ -1,4 +1,3 @@
 Humpty Dumpty sat on a wall, Humpty Dumpty had a great fall. All the King’s horses and all the
 King’s men, Couldn’t put Humpty together again.
 
-![Humpty dumpty image](/src/assets/humpty-dumpty.jpg)
--- src/routes/nursery-rhymes.$postId.tsx
+++ src/routes/nursery-rhymes.$postId.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { Link, createFileRoute } from "@tanstack/react-router";
 import { css } from "../../stitches.config";
 import Markdown from 'markdown-to-jsx'
 import { useEffect, useState } from "react";
@@ -12,6 +12,8 @@ export const Route = createFileRoute('/nursery-rhymes/$postId')({
 function NurseryRhymePost() {
     const { postId } = Route.useParams()
     const [post, setPost] = useState('')
+    const rhymes = ['humpty dumpty', 'sailor went to sea', 'baa baa black sheep', 'bingo', 'alphabet song']
+
     useEffect(() => {
         import(`../rhymes/${postId}.md`)
             .then(res => {
@@ -21,6 +23,16 @@ function NurseryRhymePost() {
     });
 
     return <section className={css({ maxWidth: '48rem', mx: 'auto', mt: 64, paddingBottom: 44, lineHeight: 2.8, fontSize: 22, fontWeight: 'semibold', '@md': { fontSize: 16, maxWidth: '32rem' }, '@sm': { maxWidth: '20rem' } })()}>
+        <p>Nursery Rhymes</p>
+        <ul className={css({ fontSize: 18 })()}>
+            {rhymes.map((item) => (
+                <li>
+                    <Link to={`nursery-rhymes/${item.split(' ').join('-')}`} from="/">
+                        {item}
+                    </Link>
+                </li>
+            ))}
+        </ul>
         <h2 className={css({ fontSize: 28, color: '$lime12', my: 12, '@md': { fontSize: 20 } })()}>
             <Balancer>
                 <p style={{ textTransform: 'capitalize' }}>{postId.split('-').join(' ')}</p>
